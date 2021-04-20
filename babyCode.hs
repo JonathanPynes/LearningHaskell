e5b26e1e00e5d42748d68163dfdbafcdb798cfc0
Jonathan Pynes <78613980+JonathanPynes@users.noreply.github.com>
2021-04-20 18:07:32 -0500

    Add files via upload

---
+++ babyCode.hs
@@ -0,0 +1,7 @@
+-- doubleMe x = x + x + x
+-- doubleUs x y = doubleMe x + doubleMe y
+-- doubleSmallNumber x = if x > 100
+--                         then x
+--                         else x*2;
+
+let lostNumbers = [4,8,15,16,23,42]
